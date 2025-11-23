42d60bd8166f5b897f150080242e1b986f2fb994
Pavel Vasilev <pavelabadon@gmail.com>
2025-11-23 13:50:16 +0200

    Add components Header and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,12 @@
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
 function App() {
   
 
   return (
     <>
       <>
-  <header>
-    {/* Navigation */}
-    <nav>
-      <a className="home" href="#">
-        {" "}
-        <img src="./images/logo.png" alt="logo" />{" "}
-      </a>
-      <a href="#">Catalog</a>
-      {/* Logged-in users */}
-      <div id="user">
-        <a href="#">Add Game</a>
-        <a href="#">Logout</a>
-      </div>
-      {/* Guest users */}
-      <div id="guest">
-        <a href="#">Login</a>
-        <a href="#">Register</a>
-      </div>
-    </nav>
-  </header>
+  <Header/>
   {/*Home Page*/}
   <section id="welcome-world">
     <div className="welcome-message">
@@ -67,6 +50,7 @@ function App() {
     </div>
   </section>
 </>
+<Footer/>
 
     </>
   )
